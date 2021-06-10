2c96fad556cb23cb65fb0d27826b0528aed9fe32
Caleb Tony <calebmbugua@gmail.com>
2021-06-10 03:46:29 +0300

    added index view function

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from rest_framework.views import APIView
+
+from .models import Category, Course, Lesson, Profile, Progress, RegisteredCourses
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def index(request):
+    pass
+
+
