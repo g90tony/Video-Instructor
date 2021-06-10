595be6ddeaf0617a6bbef217792b51cf8da3f53c
Caleb Tony <calebmbugua@gmail.com>
2021-06-10 03:45:05 +0300

    added single model serializers

---
+++ courses/serializers.py
@@ -0,0 +1,74 @@
+from django.contrib.auth.models import User
+from django.db.models import fields
+from rest_framework import serializers
+
+from .models import Category, Course, Lesson, Profile, Progress, RegisteredCourses
+
+class UserSerializer(serializers.ModelField):
+    class Meta:
+        model = User
+        fields = ("username","email",)
+        
+        
+class CategorySerializer(serializers.ModelField):
+    class Meta:
+        model = Category
+        fields = ("name")
+        
+class CourseSerializer(serializers.ModelField):
+    
+    thumbnail = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Course
+        fields = ("name", "description", "category", "thumbnail",) 
+        
+    def get_thumbnail(self, course):
+        request = self.context.get('request')
+        thumbnail = course.thumbnail.url
+        
+        return request.build_absolute_uri(thumbnail)
+    
+    
+class LessonSerializer(serializers.ModelField):
+    
+    course = CourseSerializer(many = False)
+    
+    class Meta:
+        model = Lesson
+        fields = ("title", "descripion", "lesson", "course",) 
+        
+        
+class ProfileSerializer(serializers.ModelField):
+    
+    avatar = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Profile
+        fields = ("avatar", "first_name", "last_name",) 
+        
+    def get_avatar(self, profile):
+        request = self.context.get('request')
+        avatar = profile.avatar.url
+        
+        return request.build_absolute_uri(avatar)
+    
+    
+class ProgressSerializer(serializers.ModelField):
+    profile = ProfileSerializer(many = False)
+    course = CourseSerializer(many = False)
+    lesson = LessonSerializer(many = False)
+    
+    class Meta:
+        model = Progress
+        fields = ("profile", "course", "lesson",) 
+        
+        
+class RegisterCourseSerializer(serializers.ModelField):
+    
+    profile = ProfileSerializer(many = False)
+    course = CourseSerializer(many = False)
+            
+    class Meta:
+        model: RegisteredCourses
+        fields = ("profile", "course",) 
