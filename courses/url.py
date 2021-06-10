331627b10b48d693a8e8ca5544c31d96c257d7a3
Caleb Tony <calebmbugua@gmail.com>
2021-06-10 05:43:41 +0300

    added api routes

--- courses/url.py
+++ courses/url.py
@@ -21,4 +21,20 @@ from . import views as API_ROUTES
 
 urlpatterns = [
     path('', view=API_ROUTES.index(), name='Home'),
+    
+    path('api/categories/', view=API_ROUTES.ListCategory.as_view()),
+    
+    path('api/courses/<int:courses_id>', view=API_ROUTES.SingleCourse.as_view()),
+    path('api/courses/', view=API_ROUTES.ListCourse.as_view()),
+    path('api/courses/search', view=API_ROUTES.SearchCourses.as_view()),
+    
+    path('api/lesson/<int:lesson_id>', view=API_ROUTES.ListLessons.as_view()),
+    
+    path('api/profile/<int:profile_id>', view=API_ROUTES.SingleProfile.as_view()),
+    path('api/profile/', view=API_ROUTES.ListProfile.as_view()),
+    
+    path('api/progress/<int:progress_id>', view=API_ROUTES.SingleProgress.as_view()),
+    path('api/progress/<int:progress_id>', view=API_ROUTES.ListProgress.as_view()),
+    
+    path('api/courses/registered', view=API_ROUTES.ListRegisterCourse.as_view()),
 ]
